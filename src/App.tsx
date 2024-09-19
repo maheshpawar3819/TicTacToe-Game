2e68c8eab825f543a026747d3f4c4c25ce8e7196
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-19 16:35:36 +0530

    add logic to check winner and click turn

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -8,12 +8,108 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import Snackbar from 'react-native-snackbar';
+import Icons from './Components/Icons';
 
 function App(): React.JSX.Element {
+  const [isCross, setIsCross] = useState<boolean>(false);
+  const [gameWinner, setGameWinner] = useState<string>('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setGameWinner('');
+    setGameState(new Array(9).fill('empty', 0, 9));
+  };
+
+  const checkIsWinner = () => {
+    //logic to checking winner of the game
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== 'empty'
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🏆🥇🥳`);
+    } else if (
+      gameState[3] !== 'empty' &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5]
+    ) {
+      setGameWinner(`${gameState[3]} won the game!🏆🥇🥳`);
+    } else if (
+      gameState[6] !== 'empty' &&
+      gameState[6] === gameState[7] &&
+      gameState[7] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[6]} won the game!🏆🥇🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[1] !== 'empty' &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7]
+    ) {
+      setGameWinner(`${gameState[1]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8]
+    ) {
+      setGameWinner(`${gameState[0]} won the game! 🥳`);
+    } else if (
+      gameState[2] !== 'empty' &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6]
+    ) {
+      setGameWinner(`${gameState[2]} won the game! 🥳`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game... ⌛️');
+    }
+  };
+
+  const onChangeItem = (itmeNumber: number) => {
+    //condition for if game is won by somebody
+    if (gameWinner) {
+      return Snackbar.show({
+        text: gameWinner,
+        backgroundColor: '#000000',
+        textColor: '#ffffff',
+      });
+    }
+    //if the game is not won by somebody
+    if (gameState[itmeNumber] === 'empty') {
+      //to check whose turn
+      gameState[itmeNumber] === isCross ? 'cross' : 'circle';
+      //flip the switch
+      setIsCross[!isCross];
+    } else {
+      return Snackbar.show({
+        text: 'Position is already fillde',
+        backgroundColor: 'red',
+        textColor: 'white',
+      });
+    }
+    //evaluate winner
+    checkIsWinner();
+  };
+
   return (
-    <View>
-      <Text>hello</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>Tic Tac Toe Game</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
